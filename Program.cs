044323629c6f608d26e0f90cd7717f0390e08f94
Alexey <birill56@gmail.com>
2023-06-01 14:23:33 +1000

    1

--- Program.cs
+++ Program.cs
@@ -1,37 +1,4 @@
-﻿// Напишите программу, которая принимает на вход координаты двух точек 
-// и находит расстояние между ними в N-мерном пространстве. 
-// Сначала задается N с клавиатуры, потом задаются координаты точек.
-int InPut(string message)
-{
-    int value = 0;
-    System.Console.WriteLine(message);
-    value = int.Parse(Console.ReadLine());
-    return value;
-}
+﻿// Задача 19 - HARD необязательная
 
-System.Console.WriteLine("Введите мерность пространства: ");
-int N = int.Parse(Console.ReadLine());
-double[] x = new double[N];
-double[] y = new double[N];
-double[] z = new double[N];
-double dist = 0;
-
-for (int i = 0; i < N; i++)
-{
-    if (i == 0) x[i] = InPut("Введите координату первой точки: ");
-    else x[i] = InPut("Введите следующую координату первой точки: ");
-
-
-}
-for (int i = 0; i < N; i++)
-{
-    if (i == 0) y[i] = InPut("Введите координату второй точки: ");
-    else y[i] = InPut("Введите следующую координату второй точки: ");
-}
-for (int i = 0; i < N; i++)
-{
-    double result = y[i] - x[i];
-    dist += result * result;
-}
-
-System.Console.WriteLine(Math.Sqrt(dist));
+// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, 
+// является ли оно палиндромом. Через строку нельзя решать само собой.
